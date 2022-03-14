8c047b6baad4a77c8784e8cb9979a2015b50db82
BuildTools <unconfigured@null.spigotmc.org>
2022-03-14 20:14:05 +0100

    First commit

---
+++ components/DownloadLatest.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="this.info">
+      <b-button type="is-success" icon-left="download" @click="downloadLatest">
+      {{this.info.name}}
+      </b-button>
+  </div>
+  <div v-else>
+      <p>Loading...</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { DialogProgrammatic as Dialog } from 'buefy'
+
+export default {
+  name: 'DownloadLatest',
+  data () {
+    return {
+      info: undefined
+    }
+  },
+  mounted () {
+    axios.get('https://api.github.com/repos/Konloch/bytecode-viewer/releases/latest')
+      .then((response) => {
+        this.info = response.data
+        console.log(this.info)
+      }).catch((err) => {
+        Dialog.alert({
+          message: 'Could not get the latest release from Github<br/> ' + err,
+          type: 'is-danger'
+        })
+      })
+  },
+  methods: {
+    downloadLatest () {
+      window.open(this.info.assets[0].browser_download_url)
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="page">
+    <div class="container">
+      <div class="is-flex is-justify-content-center">
+        <div class="">
+        </div>
+        <div class="">
+          <div class="box">
+            <DownloadLatest/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+html, #page{
+  width: 100%;
+  height: 1000;
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+}
+</style>
